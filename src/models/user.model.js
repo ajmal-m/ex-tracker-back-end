2c482b9728107e919e5ec54d544f5e99156ee2f0
ajmal-m <ajua76774@gmail.com>
2025-11-20 10:52:47 +0530

    User schema updates

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
-    name: String,
-    email:String,
-    password: String
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
 },  { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
